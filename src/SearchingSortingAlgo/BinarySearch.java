1fc6579a246df113dae8ae6a70a113957391d0b7
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-11-02 10:34:19 +0530

    Searching and Sorting Algo

--- src/SearchingSortingAlgo/BinarySearch.java
+++ src/SearchingSortingAlgo/BinarySearch.java
@@ -1,2 +1,22 @@
-package SearchingSortingAlgo;public class BinarySearch {
+package SearchingSortingAlgo;
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int x) {
+
+        int n = arr.length;
+        int s = 0;
+        int e = n - 1;
+        int mid;
+        while (s <= e) {
+            mid = (s + e) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] > x) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
--- src/SearchingSortingAlgo/SortingAlgo.java
+++ src/SearchingSortingAlgo/SortingAlgo.java
@@ -1,2 +1,56 @@
-package SearchingSortingAlgo;public class SortingAlgo {
+package SearchingSortingAlgo;
+
+public class SortingAlgo {
+
+    static void selectionSort(int[] arr) {
+
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            //insert element at ith position
+            int min = Integer.MAX_VALUE;
+            int minIdx = -1;
+            for (int j = i; j < n; j++) {
+                //finding the minimum element among j positions
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIdx = j;
+                }
+            }
+            //swap element at minIdx with ith element
+            int t = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = t;
+        }
+    }
+
+    static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            // n-1 rounds
+            for (int j = 0; j < n - 1 - i; j++) {
+                // n-i-1 the last element is at correct pos at each iteration
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+    }
+
+    static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) System.out.print(arr[i] + ",");
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 6, 4, 8, 0, 3};
+        selectionSort(arr);
+        printArray(arr);
+        System.out.println();
+        bubbleSort(arr);
+        printArray(arr);
+    }
 }
